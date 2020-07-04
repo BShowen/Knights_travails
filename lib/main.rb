e6771eb35970500bc9eab3018f9ae8d9c05b3dd7
Bradley Showen <bshowen@me.com>
2020-07-04 15:33:12 -0400

    adds visualization to show grph and path

--- lib/main.rb
+++ lib/main.rb
@@ -8,14 +8,22 @@ require "./knight.rb"
 board = Board.new
 knight = Knight.new
 
-knight.current_position = [0,0]
-knight.desired_position = [0,7]
+# knight.current_position = [0,0]
+puts board
+puts "Enter a starting cordinate. For example, enter 00 for (0,0): "
+starting_coord = gets.chomp.split("").map(&:to_i)
+knight.current_position = starting_coord
+puts "Enter an ending coordinate. For example, enter 77 for (7,7): "
+ending_coord = gets.chomp.split("").map(&:to_i)
+knight.desired_position = ending_coord
 knight.knight_moves(knight.current_position,knight.desired_position)
 knight.visited_coordinates.each do |coordinate|
     board.square(coordinate).character = "*"
     puts `clear`
     puts board
-    sleep 0.2
+    puts "press enter for next move"
+    gets.chomp
+    # sleep 0.2
 end
 knight.final_coordinates.each_with_index do |coordinate, index|
     board.square(coordinate).character = index.to_s
