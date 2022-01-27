5790189b89332aa25dc7a5674560e6f14716e585
Bradley Showen <bshowen@live.seminolestate.edu>
2022-01-27 14:31:04 -0500

    graph shows moves happening

--- lib/main.rb
+++ lib/main.rb
@@ -18,16 +18,19 @@ ending_coord = gets.chomp.split("").map(&:to_i)
 knight.desired_position = ending_coord
 knight.knight_moves(knight.current_position,knight.desired_position)
 knight.visited_coordinates.each do |coordinate|
-    board.square(coordinate).character = "*"
-    puts `clear`
-    puts board
-    puts "press enter for next move"
-    gets.chomp
+    # board.square(coordinate).character = "*"
+    # puts `clear`
+    # puts board
+    # puts "press enter for next move"
+    # gets.chomp
     # sleep 0.2
 end
 knight.final_coordinates.each_with_index do |coordinate, index|
     board.square(coordinate).character = index.to_s
     board.square(coordinate).color = :blue
+    sleep 0.5
+    puts `clear`
+    puts board
 end
 puts `clear`
 puts board
